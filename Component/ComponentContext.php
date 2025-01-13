e8d4d9f33b34671b5bea645681e0098d1b09c03f
Jisse Reitsma <jisse@yireo.com>
2025-01-13 10:52:48 +0100

    Redirect if there are no more items in the cart

--- Component/ComponentContext.php
+++ Component/ComponentContext.php
@@ -4,16 +4,23 @@ declare(strict_types=1);
 namespace Yireo\LokiComponents\Component;
 
 use Magento\Customer\Model\Session as CustomerSession;
+use Magento\Framework\UrlFactory;
 
 class ComponentContext implements ComponentContextInterface
 {
     public function __construct(
-        private CustomerSession $customerSession
+        private CustomerSession $customerSession,
+        private readonly UrlFactory $urlFactory,
     ) {
     }
 
     public function getCustomerSession(): CustomerSession
     {
         return $this->customerSession;
+    }
+
+    public function getUrlFactory(): UrlFactory
+    {
+        return $this->urlFactory;
     }
 }

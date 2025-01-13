6ac096b332a8ab9e08e6410ae0f3b94221183f54
Jisse Reitsma <jisse@yireo.com>
2025-01-13 10:44:17 +0100

    Allow for JSON-based redirects

--- Controller/Index/Html.php
+++ Controller/Index/Html.php
@@ -5,6 +5,8 @@ declare(strict_types=1);
 namespace Yireo\LokiComponents\Controller\Index;
 
 use Exception;
+use Magento\Framework\Controller\Result\Json as JsonResult;
+use Magento\Framework\Controller\Result\JsonFactory as JsonResultFactory;
 use Magento\Framework\View\LayoutInterface;
 use Magento\Framework\App\Action\HttpGetActionInterface;
 use Magento\Framework\App\Action\HttpPostActionInterface;
@@ -14,6 +16,7 @@ use Magento\Framework\View\Element\AbstractBlock;
 use Yireo\LokiComponents\Controller\HtmlResult;
 use Yireo\LokiComponents\Controller\HtmlResultFactory;
 use Yireo\LokiComponents\Exception\NoComponentFoundException;
+use Yireo\LokiComponents\Exception\RedirectException;
 use Yireo\LokiComponents\Messages\GlobalMessageRegistry;
 use Yireo\LokiComponents\Util\Controller\LayoutLoader;
 use Yireo\LokiComponents\Util\Controller\RepositoryDispatcher;
@@ -28,6 +31,7 @@ class Html implements HttpPostActionInterface, HttpGetActionInterface
         private readonly RepositoryDispatcher $repositoryDispatcher,
         private readonly TargetRenderer $targetRenderer,
         private readonly HtmlResultFactory $htmlResultFactory,
+        private readonly JsonResultFactory $jsonResultFactory,
         private readonly GlobalMessageRegistry $globalMessageRegistry,
     ) {
     }
@@ -42,12 +46,16 @@ class Html implements HttpPostActionInterface, HttpGetActionInterface
                 $this->getBlock($layout, $data['block']),
                 $data['componentData']
             );
-        } catch(Exception $exception) {
+        } catch (RedirectException $redirectException) {
+            return $this->getJsonRedirect($redirectException->getMessage());
+        } catch (Exception $exception) {
             $this->globalMessageRegistry->addError($exception->getMessage());
+
             return $this->getHtmlResult();
         }
 
         $htmlParts = $this->targetRenderer->render($layout, $data['targets']);
+
         return $this->getHtmlResult($htmlParts);
     }
 
@@ -78,5 +86,16 @@ class Html implements HttpPostActionInterface, HttpGetActionInterface
         $htmlResult->setContents($html);
 
         return $htmlResult;
+    }
+
+    private function getJsonRedirect(string $redirectUrl): JsonResult
+    {
+        $json =$this->jsonResultFactory->create()->setData([
+            'redirect' => $redirectUrl,
+        ]);
+
+        $json->setHeader('X-Loki-Redirect', $redirectUrl);
+
+        return $json;
     }
 }
---
+++ Exception/RedirectException.php
@@ -0,0 +1,9 @@
+<?php declare(strict_types=1);
+
+namespace Yireo\LokiComponents\Exception;
+
+use RuntimeException;
+
+class RedirectException extends RuntimeException
+{
+}

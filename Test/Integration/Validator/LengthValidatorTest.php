c5c5ff215d2b19433f7296e6ec6c1d57ed2cd36c
Jisse Reitsma <jisse@yireo.com>
2025-01-22 11:32:49 +0100

    Cleanup

--- Test/Integration/Validator/LengthValidatorTest.php
+++ Test/Integration/Validator/LengthValidatorTest.php
@@ -2,21 +2,11 @@
 
 namespace Yireo\LokiComponents\Test\Integration\Validator;
 
-use Magento\Customer\Api\CustomerRepositoryInterface;
-use Magento\Customer\Model\AccountManagement;
 use Magento\Framework\App\ObjectManager;
-use Magento\Framework\DataObject;
-use Magento\Framework\Exception\LocalizedException;
-use Magento\Framework\Exception\NoSuchEntityException;
 use Magento\TestFramework\Fixture\AppArea;
-use Magento\TestFramework\Fixture\Config;
 use PHPUnit\Framework\TestCase;
 use Yireo\LokiCheckout\Component\Base\Field\FieldViewModel;
-use Yireo\LokiComponents\Component\Component;
 use Yireo\LokiComponents\Component\ComponentInterface;
-use Yireo\LokiComponents\Component\ComponentViewModel;
-use Yireo\LokiComponents\Component\ComponentViewModelInterface;
-use Yireo\LokiComponents\Validator\EmailValidator;
 use Yireo\LokiComponents\Validator\LengthValidator;
 
 #[AppArea('frontend')]

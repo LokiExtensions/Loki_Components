5c808809ffd90760ab5f784d34c7fea3136b8e27
Jisse Reitsma <jisse@yireo.com>
2025-01-30 13:11:10 +0100

    Move CssClass to LokiComponents

--- Observer/AssignAdditionalBlockVariables.php
+++ Observer/AssignAdditionalBlockVariables.php
@@ -7,11 +7,13 @@ use Magento\Framework\Event\Observer;
 use Magento\Framework\Event\ObserverInterface;
 use Magento\Framework\View\Element\Template;
 use Yireo\LokiComponents\Factory\ViewModelFactory;
+use Yireo\LokiComponents\Util\Block\CssClassFactory;
 
 class AssignAdditionalBlockVariables implements ObserverInterface
 {
     public function __construct(
         private readonly ViewModelFactory $viewModelFactory,
+        private readonly CssClassFactory $cssClassFactory,
     ) {
     }
 
@@ -21,6 +23,8 @@ class AssignAdditionalBlockVariables implements ObserverInterface
         if (false === $block instanceof Template) {
             return;
         }
+
+        $block->assign('css', $this->cssClassFactory->create()->setBlock($block));
 
         $block->assign('viewModelFactory', $this->viewModelFactory);
     }

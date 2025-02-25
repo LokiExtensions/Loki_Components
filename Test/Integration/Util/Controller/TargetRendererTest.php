06d073c360145593bd2d2d27381ba40febf61b6e
Jisse Reitsma <jisse@yireo.com>
2025-02-25 14:57:08 +0100

    Add new incomplete test

---
+++ Test/Integration/Util/Controller/TargetRendererTest.php
@@ -0,0 +1,13 @@
+<?php declare(strict_types=1);
+
+namespace Yireo\LokiComponents\Util\Controller;
+
+use PHPUnit\Framework\TestCase;
+
+class TargetRendererTest extends TestCase
+{
+    public function testRender()
+    {
+        $this->markTestIncomplete();
+    }
+}

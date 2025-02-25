05f2cbdd5d6481b6585aaa7ff2f256dc921c8b42
Jisse Reitsma <jisse@yireo.com>
2025-02-25 14:19:30 +0100

    Move EmailValidatorTest

--- Test/Integration/Validator/EmailValidatorTest.php
+++ Test/Integration/Validator/EmailValidatorTest.php
@@ -2,16 +2,10 @@
 
 namespace Yireo\LokiComponents\Test\Integration\Validator;
 
-use Magento\Customer\Api\CustomerRepositoryInterface;
-use Magento\Customer\Model\AccountManagement;
 use Magento\Framework\App\ObjectManager;
-use Magento\Framework\Exception\LocalizedException;
-use Magento\Framework\Exception\NoSuchEntityException;
 use Magento\TestFramework\Fixture\AppArea;
-use Magento\TestFramework\Fixture\Config;
 use PHPUnit\Framework\TestCase;
 use Yireo\LokiComponents\Validator\EmailValidator;
-use Magento\Customer\Api\Data\CustomerInterfaceFactory;
 
 #[AppArea('frontend')]
 class EmailValidatorTest extends TestCase
@@ -49,36 +43,5 @@ class EmailValidatorTest extends TestCase
             ['jane', 'Invalid email'],
             ['jane@example.comcomcomcomcom', 'does not seem to be valid'],
         ];
-    }
-
-    #[Config(AccountManagement::GUEST_CHECKOUT_LOGIN_OPTION_SYS_CONFIG, 1, 'website')]
-    public function testWithExistingEmail(): void
-    {
-        $this->createCustomer('john@example.com');
-
-        $validator = ObjectManager::getInstance()->get(EmailValidator::class);
-        $email = 'john@example.com';
-        $result = $validator->validate($email);
-
-        $this->assertNotTrue($result);
-        $this->assertContains('This email address is not available', $result);
-    }
-
-    private function createCustomer(string $email)
-    {
-        $customerRepository = ObjectManager::getInstance()->get(CustomerRepositoryInterface::class);
-        try {
-            $customerRepository->get($email);
-            return;
-        } catch (NoSuchEntityException $e) {
-        } catch (LocalizedException $e) {
-        }
-
-        $customerFactory = ObjectManager::getInstance()->get(CustomerInterfaceFactory::class);
-        $customer = $customerFactory->create();
-        $customer->setFirstname('John');
-        $customer->setLastname('Doe');
-        $customer->setEmail($email);
-        $customerRepository->save($customer);
     }
 }

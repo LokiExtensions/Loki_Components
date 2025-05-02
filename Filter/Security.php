0ce84604f8c6e6942b55a3eaf4118a9eb99eb19d
Jisse Reitsma <jisse@yireo.com>
2025-05-02 09:04:15 +0200

    Support security for complex values

--- Filter/Security.php
+++ Filter/Security.php
@@ -6,6 +6,16 @@ class Security implements FilterInterface
 {
     public function filter(mixed $value): mixed
     {
+       if (is_object($value)) {
+            return $value;
+        }
+
+        if (is_array($value)) {
+            foreach ($value as &$val) {
+                $val = $this->filter($val);
+            }
+        }
+
         $value = (string)$value;
         $value = strip_tags($value);
         $value = htmlentities($value);

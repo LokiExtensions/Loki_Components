a43dbe3710c153d8763e1e54d6c9aecfcd892d89
Jisse Reitsma <jisse@yireo.com>
2025-09-02 08:54:48 +0200

    Remove LengthBehaviourInterface entirely

--- Validator/LengthValidator.php
+++ Validator/LengthValidator.php
@@ -2,7 +2,6 @@
 
 namespace Loki\Components\Validator;
 
-use Loki\Components\Component\Behaviour\LengthBehaviourInterface;
 use Loki\Components\Component\ComponentInterface;
 
 class LengthValidator implements ValidatorInterface
@@ -14,17 +13,13 @@ class LengthValidator implements ValidatorInterface
         }
 
         $viewModel = $component->getViewModel();
-        if (false === $viewModel instanceof LengthBehaviourInterface) {
-            return true;
-        }
-
         $minlength = (int)$viewModel->getBlock()->getMinlength();
         if ($minlength > 0 && strlen($value) < $minlength) {
             return [__('Value should have more than %1 characters', $minlength)];
         }
 
         $maxlength = (int)$viewModel->getBlock()->getMaxlength();
-        if ($maxlength && strlen($value) > $maxlength) {
+        if ($maxlength > 0 && strlen($value) > $maxlength) {
             return [__('Value should have no more than %1 characters', $maxlength)];
         }
 

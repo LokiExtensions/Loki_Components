a3fce0dee9ca7e5fa0cc5c1be05002114cda5036
Jisse Reitsma <jisse@yireo.com>
2025-11-27 09:21:06 +0100

    Fix unit test

--- Test/Unit/Validator/RequiredValidatorTest.php
+++ Test/Unit/Validator/RequiredValidatorTest.php
@@ -3,6 +3,7 @@
 namespace Loki\Components\Test\Unit\Validator;
 
 use Loki\Components\Component\ComponentInterface;
+use Loki\Components\Util\Ajax;
 use Loki\Components\Util\IsEmpty;
 use PHPUnit\Framework\TestCase;
 use Loki\Components\Validator\RequiredValidator;
@@ -12,8 +13,10 @@ class RequiredValidatorTest extends TestCase
     public function testWithNoSimpleValues(): void
     {
         $component = $this->createMock(ComponentInterface::class);
+        $ajax = $this->createMock(Ajax::class);
+        $ajax->method('isEmpty')->willReturn(true);
 
-        $validator = new RequiredValidator(new IsEmpty());
+        $validator = new RequiredValidator(new IsEmpty(), $ajax);
         $this->assertTrue($validator->validate(1, $component));
         $this->assertIsArray($validator->validate(0, $component));
         $this->assertIsArray($validator->validate('', $component));

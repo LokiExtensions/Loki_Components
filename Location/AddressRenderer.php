2850f4a015f6f41f25719e23cb6cbd2ebe81e1df
Jisse Reitsma <jisse@yireo.com>
2026-02-13 10:22:37 +0100

    Add oneline renderer to AddressRenderer

--- Location/AddressRenderer.php
+++ Location/AddressRenderer.php
@@ -12,19 +12,51 @@ class AddressRenderer
     ) {
     }
 
+    public function getLine(Address $address): string
+    {
+        $renderer = $this->getRenderer('oneline');
+        $text = $renderer->renderArray($this->getAddressDataFromAddress($address));
+        $newTextParts = [];
+        $textParts = explode(',', $text);
+        foreach ($textParts as $textPart) {
+            $textPart = trim($textPart);
+            if (!empty($textPart)) {
+                $newTextParts[] = $textPart;
+            }
+        }
+
+        return implode(', ', $newTextParts);
+    }
+
     public function getHtml(Address $address): string
     {
-        $addressData = [
-            'street' => [$address->getStreet(), $address->getHouseNumber()],
+        $renderer = $this->getRenderer('html');
+        $html = $renderer->renderArray($this->getAddressDataFromAddress($address));
+        $html = preg_replace('/^<br(\s+)\/>$/msi', '', $html);
+        return $html;
+    }
+
+    private function getRenderer(string $code = 'html'): RendererInterface
+    {
+        return $this->addressConfig->getFormatByCode($code)->getRenderer();
+    }
+
+    private function getAddressDataFromAddress(Address $address): array
+    {
+        $data = [
+            'prefix' => $address->getPrefix(),
+            'firstname' => $address->getFirstname(),
+            'middlename' => $address->getMiddlename(),
+            'lastname' => $address->getLastname(),
+            'suffix' => $address->getSuffix(),
+            'company' => $address->getCompany(),
+            'street' => [$address->getStreet(), $address->getHouseNumber(), $address->getHouseNumberAddition()],
             'postcode' => $address->getPostcode(),
             'city' => $address->getCity(),
+            'region' => $address->getRegion(),
             'country_id' => $address->getCountryId(),
         ];
 
-        /** @var RendererInterface $renderer */
-        $renderer = $this->addressConfig->getFormatByCode('html')->getRenderer();
-        $html = $renderer->renderArray($addressData);
-        $html = preg_replace('/^<br(\s+)\/>$/msi', '', $html);
-        return $html;
+        return $data;
     }
 }

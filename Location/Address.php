6ab256220cdc3f1ca4cc7d27306650254985b9f9
Jisse Reitsma <jisse@yireo.com>
2026-02-13 10:22:02 +0100

    refactor: Add numerous fields to Address-object

--- Location/Address.php
+++ Location/Address.php
@@ -8,20 +8,123 @@ class Address extends DataObject
 {
     public function __construct(
         private readonly AddressRenderer $addressRenderer,
-        private readonly string $street,
-        private readonly string $houseNumber,
-        private readonly string $houseNumberAddition,
-        private readonly string $postcode,
-        private readonly string $city,
-        private readonly string $countryId,
-        private readonly ?float $latitude = null,
-        private readonly ?float $longitude = null,
-        private readonly string $phone = '',
-        private readonly string $comment = '',
-        private readonly string $company = '',
+        private string $street = '',
+        private string $houseNumber = '',
+        private string $houseNumberAddition = '',
+        private string $postcode = '',
+        private string $city = '',
+        private string $region = '',
+        private string $countryId = '',
+        private ?float $latitude = null,
+        private ?float $longitude = null,
+        private string $telephone = '',
+        private string $fax = '',
+        private string $vatId = '',
+        private string $comment = '',
+        private string $company = '',
+        private string $prefix = '',
+        private string $firstname = '',
+        private string $middlename = '',
+        private string $lastname = '',
+        private string $suffix = '',
         array $data = []
     ) {
         parent::__construct($data);
+    }
+
+    public function setStreet(string $street): void
+    {
+        $this->street = $street;
+    }
+
+    public function setHouseNumber(string $houseNumber): void
+    {
+        $this->houseNumber = $houseNumber;
+    }
+
+    public function setHouseNumberAddition(string $houseNumberAddition): void
+    {
+        $this->houseNumberAddition = $houseNumberAddition;
+    }
+
+    public function setPostcode(string $postcode): void
+    {
+        $this->postcode = $postcode;
+    }
+
+    public function setCity(string $city): void
+    {
+        $this->city = $city;
+    }
+
+    public function setRegion(string $region): void
+    {
+        $this->region = $region;
+    }
+
+    public function setCountryId(string $countryId): void
+    {
+        $this->countryId = $countryId;
+    }
+
+    public function setLatitude(?float $latitude): void
+    {
+        $this->latitude = $latitude;
+    }
+
+    public function setLongitude(?float $longitude): void
+    {
+        $this->longitude = $longitude;
+    }
+
+    public function setTelephone(string $telephone): void
+    {
+        $this->telephone = $telephone;
+    }
+
+    public function setFax(string $fax): void
+    {
+        $this->fax = $fax;
+    }
+
+    public function setVatId(string $vatId): void
+    {
+        $this->vatId = $vatId;
+    }
+
+    public function setComment(string $comment): void
+    {
+        $this->comment = $comment;
+    }
+
+    public function setCompany(string $company): void
+    {
+        $this->company = $company;
+    }
+
+    public function setPrefix(string $prefix): void
+    {
+        $this->prefix = $prefix;
+    }
+
+    public function setFirstname(string $firstname): void
+    {
+        $this->firstname = $firstname;
+    }
+
+    public function setMiddlename(string $middlename): void
+    {
+        $this->middlename = $middlename;
+    }
+
+    public function setLastname(string $lastname): void
+    {
+        $this->lastname = $lastname;
+    }
+
+    public function setSuffix(string $suffix): void
+    {
+        $this->suffix = $suffix;
     }
 
     public function getStreet(): string
@@ -64,6 +167,11 @@ class Address extends DataObject
         return $this->longitude;
     }
 
+    public function getAddressLine(): string
+    {
+        return $this->addressRenderer->getLine($this);
+    }
+
     public function getInnerHtml(): string
     {
         return $this->addressRenderer->getHtml($this);
@@ -79,8 +187,48 @@ class Address extends DataObject
         return $this->houseNumberAddition;
     }
 
-    public function getPhone(): string
+    public function getPrefix(): string
     {
-        return $this->phone;
+        return $this->prefix;
+    }
+
+    public function getFirstname(): string
+    {
+        return $this->firstname;
+    }
+
+    public function getMiddlename(): string
+    {
+        return $this->middlename;
+    }
+
+    public function getLastname(): string
+    {
+        return $this->lastname;
+    }
+
+    public function getSuffix(): string
+    {
+        return $this->suffix;
+    }
+
+    public function getRegion(): string
+    {
+        return $this->region;
+    }
+
+    public function getTelephone(): string
+    {
+        return $this->telephone;
+    }
+
+    public function getFax(): string
+    {
+        return $this->fax;
+    }
+
+    public function getVatId(): string
+    {
+        return $this->vatId;
     }
 }
--- Location/AddressFactory.php
+++ Location/AddressFactory.php
@@ -3,41 +3,82 @@
 namespace Loki\Components\Location;
 
 use Magento\Framework\ObjectManagerInterface;
+use Magento\Quote\Api\Data\AddressInterface;
 
 class AddressFactory
 {
     public function __construct(
-        private readonly ObjectManagerInterface $objectManager
+        private readonly ObjectManagerInterface $objectManager,
+        private readonly AddressParserComposite $addressParserComposite
     ) {
     }
 
     public function create(
-        string $street,
-        string $houseNumber,
-        string $houseNumberAddition,
-        string $postcode,
-        string $city,
-        string $countryId,
+        string $street = '',
+        string $houseNumber = '',
+        string $houseNumberAddition = '',
+        string $postcode = '',
+        string $city = '',
+        string $region = '',
+        string $countryId = '',
         ?float $latitude = null,
         ?float $longitude = null,
-        string $phone = '',
+        string $telephone = '',
+        string $fax = '',
+        string $vatId = '',
         string $comment = '',
         string $company = '',
+        string $prefix = '',
+        string $firstname = '',
+        string $middlename = '',
+        string $lastname = '',
+        string $suffix = '',
         array $data = [],
     ): Address {
-        return $this->objectManager->create(Address::class, [
+        $address = $this->objectManager->create(Address::class, [
             'street' => $street,
             'houseNumber' => $houseNumber,
             'houseNumberAddition' => $houseNumberAddition,
             'postcode' => $postcode,
             'city' => $city,
+            'region' => $region,
             'countryId' => $countryId,
             'latitude' => $latitude,
             'longitude' => $longitude,
-            'phone' => $phone,
+            'telephone' => $telephone,
+            'fax' => $fax,
+            'vatId' => $vatId,
             'comment' => $comment,
             'company' => $company,
+            'prefix' => $prefix,
+            'firstname' => $firstname,
+            'middlename' => $middlename,
+            'lastname' => $lastname,
+            'suffix' => $suffix,
             'data' => $data,
         ]);
+
+        return $this->addressParserComposite->parse($address);
+    }
+
+    public function createFromQuoteAddress(AddressInterface $quoteAddress): Address
+    {
+        return $this->create(
+            street:  (string)$quoteAddress->getStreetLine(1),
+            houseNumber:  (string)$quoteAddress->getStreetLine(2),
+            houseNumberAddition:  (string)$quoteAddress->getStreetLine(3) ?? '',
+            postcode:  (string)$quoteAddress->getPostcode(),
+            city: (string)$quoteAddress->getCity(),
+            countryId:  (string)$quoteAddress->getCountryId(),
+            telephone:  (string)$quoteAddress->getTelephone(),
+            fax:  (string)$quoteAddress->getFax(),
+            vatId:  (string)$quoteAddress->getVatId(),
+            company:  (string)$quoteAddress->getCompany(),
+            prefix:  (string)$quoteAddress->getPrefix(),
+            firstname:  (string)$quoteAddress->getFirstname(),
+            middlename:  (string)$quoteAddress->getMiddlename(),
+            lastname:  (string)$quoteAddress->getLastname(),
+            suffix:  (string)$quoteAddress->getSuffix(),
+        );
     }
 }

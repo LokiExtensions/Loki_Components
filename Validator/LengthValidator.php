c576ec99ee7c64ecd542b49f1939b9d2252d2e70
Jisse Reitsma <jisse@yireo.com>
2026-04-20 09:12:33 +0200

    Make sure LengthValidator also reads from min / max

--- Validator/LengthValidator.php
+++ Validator/LengthValidator.php
@@ -4,6 +4,7 @@ namespace Loki\Components\Validator;
 
 use Loki\Components\Component\ComponentInterface;
 use Loki\Components\Component\ComponentViewModelInterface;
+use Magento\Framework\View\Element\AbstractBlock;
 
 class LengthValidator implements ValidatorInterface
 {
@@ -18,16 +19,50 @@ class LengthValidator implements ValidatorInterface
             return true;
         }
 
-        $minlength = (int)$viewModel->getBlock()->getMinlength();
+        $minlength = $this->getMin($viewModel->getBlock());
         if ($minlength > 0 && strlen($value) < $minlength) {
             return [__('Value should have more than %1 characters', $minlength)];
         }
 
-        $maxlength = (int)$viewModel->getBlock()->getMaxlength();
+        $maxlength = $this->getMax($viewModel->getBlock());
         if ($maxlength > 0 && strlen($value) > $maxlength) {
             return [__('Value should have no more than %1 characters', $maxlength)];
         }
 
         return true;
+    }
+
+    private function getMin(AbstractBlock $block): int
+    {
+        $possibleValues = ['min', 'minlength'];
+        foreach ($possibleValues as $possibleValue) {
+            if ((int)$block->getData($possibleValue) > 0) {
+                return (int)$block->getData($possibleValue);
+            }
+
+            $fieldAttributes = (array)$block->getFieldAttributes();
+            if (!empty($fieldAttributes[$possibleValue])) {
+                return $fieldAttributes[$possibleValue];
+            }
+        }
+
+        return 0;
+    }
+
+    private function getMax(AbstractBlock $block): int
+    {
+        $possibleValues = ['max', 'maxlength'];
+        foreach ($possibleValues as $possibleValue) {
+            if ((int)$block->getData($possibleValue) > 0) {
+                return (int)$block->getData($possibleValue);
+            }
+
+            $fieldAttributes = (array)$block->getFieldAttributes();
+            if (!empty($fieldAttributes[$possibleValue])) {
+                return $fieldAttributes[$possibleValue];
+            }
+        }
+
+        return 0;
     }
 }

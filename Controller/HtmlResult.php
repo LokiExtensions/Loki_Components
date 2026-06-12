4c86bccd7dc3382089f8fb0ebac22c3c3958a631
Jisse Reitsma <jisse@yireo.com>
2026-06-12 13:29:54 +0200

    Autorefresh form key via AJAX calls

--- Controller/HtmlResult.php
+++ Controller/HtmlResult.php
@@ -4,11 +4,18 @@ namespace Loki\Components\Controller;
 
 use Magento\Framework\App\Response\HttpInterface as HttpResponseInterface;
 use Magento\Framework\Controller\Result\Raw;
+use Magento\Framework\Data\Form\FormKey;
 
 class HtmlResult extends Raw
 {
+    public function __construct(
+        private readonly FormKey $formKey
+    ) {
+    }
+
     protected function render(HttpResponseInterface $response)
     {
+        $response->setHeader('X-Magento-Form-Key', $this->formKey->getFormKey());
         $response->setHeader('Content-Type', 'text/html');
 
         return parent::render($response);

5d05d800998ad82bd9e6cd3035ff66f3f8e4c952
Jisse Reitsma <jisse@yireo.com>
2026-07-01 10:53:35 +0200

    style: Remove `final` keyword

--- Attribute/JsProperty.php
+++ Attribute/JsProperty.php
@@ -5,7 +5,7 @@ namespace Loki\Components\Attribute;
 use Attribute;
 
 #[Attribute(Attribute::TARGET_METHOD)]
-final class JsProperty
+class JsProperty
 {
     public function __construct(
         public readonly string $name,

bc5860519bb5d1453d30a7cbc2436db81a40ae2c
Jisse Reitsma <jisse@yireo.com>
2026-09-04 11:37:04 +0200

    chore: Rename deprecated AbstractRenderer usage to current one

--- Util/Block/BlockRenderer.php
+++ Util/Block/BlockRenderer.php
@@ -5,6 +5,7 @@ namespace Loki\Components\Util\Block;
 use InvalidArgumentException;
 use Magento\Framework\View\Element\AbstractBlock;
 use Magento\Framework\View\Element\Template;
+use Loki\Base\ViewModel\Block\AbstractRenderer;
 use RuntimeException;
 
 class BlockRenderer extends AbstractRenderer
